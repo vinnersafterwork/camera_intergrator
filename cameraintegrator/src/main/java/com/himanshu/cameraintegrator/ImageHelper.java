67af04762e22dc334ab6e1cb69987258fdfedb8b
Himanshu <him.khati@gmail.com>
2019-02-06 11:11:25 +0530

    Unecessary Funtions removed

--- cameraintegrator/src/main/java/com/himanshu/cameraintegrator/ImageHelper.java
+++ cameraintegrator/src/main/java/com/himanshu/cameraintegrator/ImageHelper.java
@@ -1,16 +1,10 @@
 package com.himanshu.cameraintegrator;
 
-import android.content.Context;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -20,6 +14,13 @@ import java.util.Date;
 public class ImageHelper {
 
 
+    /**
+     * Saves #bitmapToSave to #fileFullPath provided
+     *
+     * @param fileFullPath path where image will be saved
+     * @param bitmapToSave image which will be saved
+     * @return if file got saved successfully or not
+     */
     public static boolean saveTo(String fileFullPath, Bitmap bitmapToSave) {
 
         FileOutputStream out = null;
@@ -43,6 +44,12 @@ public class ImageHelper {
     }
 
 
+    /**
+     * Creates An Image File in $directoryName
+     *
+     * @param directoryName
+     * @return
+     */
     public static File createImageFile(String directoryName) {
 
         // Create a unique file name for image
@@ -51,11 +58,22 @@ public class ImageHelper {
         return createImageFile(directoryName, imageFileName, "jpg");
     }
 
+    /**
+     * Creates An Image File in $directoryName with name #imageName
+     *
+     * @param directoryName
+     * @return
+     */
     public static File createImageFile(String directoryName, String imageName) {
-
         return createImageFile(directoryName, imageName, "jpg");
     }
 
+    /**
+     * Creates An Image File in $directoryName with $imageName name and $fileFormat format
+     *
+     * @param directoryName
+     * @return
+     */
     public static File createImageFile(String directoryName, @Nullable String imageName, @NonNull String fileFormat) {
 
         // Getting a reference to Target storage directory
@@ -68,68 +86,5 @@ public class ImageHelper {
         return image;
     }
 
-
-    public static Bitmap scaleImageAtOptimum(Bitmap imageToScale) {
-
-        ByteArrayOutputStream bytearrayoutputstream;
-        byte[] BYTE;
-
-        final long targetArea = 1228800;
-        final int targetHeightPortrait = 1280;
-        final int targetHeightLandscape = 960;
-
-        float targetWidthPortrait;
-        float targetWidthLandscape;
-
-        long currentArea = 0;
-        float currentToTargetRatio;
-
-        if (imageToScale == null) {
-            return null;
-        } else {
-            bytearrayoutputstream = new ByteArrayOutputStream();
-            currentArea = imageToScale.getWidth() * imageToScale.getHeight();
-
-            if (currentArea < targetArea)
-                return imageToScale;
-            else {
-                Bitmap tempBitmap;
-                if (imageToScale.getHeight() > imageToScale.getWidth()) {
-                    //Height is more than width hence picture was clicked in portrait mode
-                    currentToTargetRatio = (float) imageToScale.getHeight() / targetHeightPortrait;
-                    targetWidthPortrait = imageToScale.getWidth() / currentToTargetRatio;
-                    tempBitmap = Bitmap.createScaledBitmap(imageToScale, (int) targetWidthPortrait, targetHeightPortrait, false);
-                } else {
-                    //Height is more than width hence picture was clicked in landscape mode
-                    currentToTargetRatio = (float) imageToScale.getWidth() / targetHeightLandscape;
-                    targetWidthLandscape = imageToScale.getHeight() / currentToTargetRatio;
-                    tempBitmap = Bitmap.createScaledBitmap(imageToScale, targetHeightLandscape, (int) targetWidthLandscape, false);
-
-                }
-
-                tempBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytearrayoutputstream);
-
-                BYTE = bytearrayoutputstream.toByteArray();
-
-                return BitmapFactory.decodeByteArray(BYTE, 0, BYTE.length);
-            }
-
-        }
-
-    }
-
-    public static String getRealPathFromURI(Context c, Uri contentURI) {
-        Cursor cursor = c.getContentResolver().query(contentURI, null, null, null, null);
-        if (cursor == null) {
-            // path
-            return contentURI.getPath();
-        } else {
-            cursor.moveToFirst();
-            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-            String path = cursor.getString(idx);
-            cursor.close();
-            return path;
-        }
-    }
 }
 
